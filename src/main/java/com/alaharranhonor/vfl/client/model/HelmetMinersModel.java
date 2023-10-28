fe5c143668baf0a42d7c2dd53993892a4070079b
Quarris <tomasz.winnicki@live.co.uk>
2023-10-28 18:16:12 +0100

    Fixed helmet model

--- src/main/java/com/alaharranhonor/vfl/client/model/HelmetMinersModel.java
+++ src/main/java/com/alaharranhonor/vfl/client/model/HelmetMinersModel.java
@@ -16,23 +16,23 @@ public class HelmetMinersModel<T extends LivingEntity> extends HumanoidModel<T> 
 
     public HelmetMinersModel(ModelPart root) {
         super(root, RenderType::entityCutout);
+        this.head.y = 20;
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition mesh = createMesh(CubeDeformation.NONE, 0);
         PartDefinition part = mesh.getRoot();
 
-        PartDefinition head = part.addOrReplaceChild("head", CubeListBuilder.create()
-            .texOffs(0, 0).addBox(-5.0F, -1.0F, -5.0F, 10.0F, 10.0F, 10.0F, new CubeDeformation(0.0F))
-            .texOffs(0, 20).addBox(-5.0F, 1.0F, -7.0F, 10.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
-            .texOffs(24, 20).addBox(-1.5F, -2.0F, -6.0F, 3.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 16.0F, 0.0F));
+        part.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-5.0F, -10.0F, -5.0F, 10.0F, 10.0F, 10.0F, new CubeDeformation(0.0F))
+            .texOffs(0, 20).addBox(-5.0F, -8.0F, -7.0F, 10.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+            .texOffs(24, 20).addBox(-1.5F, -11.0F, -6.0F, 3.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -10.0F, 0.0F));
 
         return LayerDefinition.create(mesh, 64, 64);
     }
 
     @Override
-    public void setupAnim(T pEntity, float pLimbSwing, float pLimbSwingAmount, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
-        super.setupAnim(pEntity, pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch);
-        this.head.y = 20;
+    public void renderToBuffer(PoseStack pPoseStack, VertexConsumer pBuffer, int pPackedLight, int pPackedOverlay, float pRed, float pGreen, float pBlue, float pAlpha) {
+        this.hat.visible = false;
+        super.renderToBuffer(pPoseStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
     }
 }
